dcbff7cc769636349d686b4ab727872c2292b7b6
Mikhail Kalinin <kalinin.mr@gmail.com>
2021-08-26 11:32:47 +0300

    user DOes not exists Test

--- src/profiles/context_processors.py
+++ src/profiles/context_processors.py
@@ -2,14 +2,21 @@ from .models import Profile, Relationship
 
 def profile_pic(request):
     if request.user.is_authenticated:
-        profile_obj = Profile.objects.get(user=request.user)
+        try:
+            profile_obj = Profile.objects.get(user__id=request.user.id)
+        except Profile.DoesNotExist:
+            return {}
         pic = profile_obj.avatar
         return {'picture':pic}
     return {}
 
+
 def invatations_received_no(request):
     if request.user.is_authenticated:
-        profile_obj = Profile.objects.get(user=request.user)
+        try:
+            profile_obj = Profile.objects.get(user__id=request.user.id)
+        except Profile.DoesNotExist:
+            return {}
         qs_count = Relationship.objects.invatations_received(profile_obj).count()
         return {'invites_num':qs_count}
     return {}
